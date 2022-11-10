b0b8f79446d68a504008405f48928fe707255f82
gul.mert89 <gul.mert89@gmail.com>
2022-11-10 15:43:37 +0300

    Update on  file

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Hey there. To get started, edit <code>src/App.js</code> and save to reload.
+          Hey there, motherfcker! To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
